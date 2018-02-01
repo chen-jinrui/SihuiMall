f3e7ff7a1c5579c7224036a553fd4165871eed6e
chen <871967481@qq.com>
2018-02-01 16:18:30 +0800

    perfect vue file

--- src/router.js
+++ src/router.js
@@ -8,43 +8,43 @@ const Goods = r => require.ensure([], () => r(require('./view/goods')), 'goods')
 Vue.use(Router);
 
 const routes = [
-    {
-        path: '*',
-        redirect: '/user'
-    }, {
-        name: 'user',
-        component: User,
-        meta: {
-            title: '会员中心'
-        }
-    }, {
-        name: 'cart',
-        component: Cart,
-        meta: {
-            title: '购物车'
-        }
-    }, {
-        name: 'goods',
-        component: Goods,
-        meta: {
-            title: '商品详情'
-        }
+  {
+    path: '*',
+    redirect: '/user'
+  }, {
+    name: 'user',
+    component: User,
+    meta: {
+      title: '会员中心'
     }
+  }, {
+    name: 'cart',
+    component: Cart,
+    meta: {
+      title: '购物车'
+    }
+  }, {
+    name: 'goods',
+    component: Goods,
+    meta: {
+      title: '商品详情'
+    }
+  }
 ];
 
-//add route path
+// add route path
 routes.forEach(route => {
-    route.path = route.path || '/' + (route.name || '');
+  route.path = route.path || '/' + (route.name || '');
 });
 
-const router = new Router({routes});
+const router = new Router({ routes });
 
 router.beforeEach((to, from, next) => {
-    const title = to.meta && to.meta.title;
-    if (title) {
-        document.title = title;
-    }
-    next();
+  const title = to.meta && to.meta.title;
+  if (title) {
+    document.title = title;
+  }
+  next();
 });
 
-export {router}
+export { router }
